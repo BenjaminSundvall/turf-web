8609303a2abc7038b86d9a49d5109df9f906cd41
Benjamin Sundvall <bensu844@student.liu.se>
2024-10-23 17:26:09 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<style>
+    :global(body) {
+        background-color: white;
+    }
+</style>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
